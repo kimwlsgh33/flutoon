3cc2a75b105cd6629a7d47f94fab861d4d620fb8
이시바 <jinho971031@gmail.com>
2023-01-09 03:29:21 +0900

    Make: pomodoros

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:toons/screens/home_screen.dart';
 
 void main(List<String> args) {
   runApp(App());
 }
 
 // make stateful widget
-class App extends StatefulWidget {
-  @override
-  State<App> createState() => _AppState();
-}
-
-// make state
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,58 +18,7 @@ class _AppState extends State<App> {
           ),
         ),
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFE5E5E5),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? MyLargeTitle() : Text("nothing"),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: Icon(
-                  Icons.remove_red_eye,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print("initState");
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print("dispose");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge!.color, // ! is not null
-      ),
+      home: HomeScreen(),
     );
   }
 }
